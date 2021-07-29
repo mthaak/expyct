e5ff44f9462d65030a1afdc24a8ddb1c4a78afff
Martin ter Haak <martin.terhaak@hummingbirdtech.com>
2021-07-29 10:34:12 +0100

    Add UUID

--- expyct/string.py
+++ expyct/string.py
@@ -25,3 +25,8 @@ class String:
         if self.ignore_case:
             flags |= re.IGNORECASE
         return flags
+
+
+UUID = String(
+    regex=re.compile("[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}")
+)
