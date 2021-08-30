cf7ce608b7aeb8f233a8d2bf6d8d6c6048c6e6d6
Martin ter Haak <martin.terhaak@hummingbirdtech.com>
2021-08-31 00:25:55 +0100

    Strip double quotes from VERSION

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from setuptools import setup
 
 with open(Path(__file__).parent / "expyct" / "__version__.py") as file:
-    VERSION = file.readline().split(" ")[2]
+    VERSION = file.readline().split(" ")[2].strip('"')
 
 if (3, 6) <= sys.version_info < (3, 7):
     INSTALL_REQUIRES = ["dataclasses", "backports-datetime-fromisoformat"]
