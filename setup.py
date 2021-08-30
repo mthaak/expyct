7445b9d54a9b811345bcdbe490c5cef6c5dbdcbe
Martin ter Haak <martin.terhaak@hummingbirdtech.com>
2021-08-30 22:50:10 +0100

    Do not test on python versions 3.5 and 3.10

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
+import sys
+
 from setuptools import setup
 
 from expyct import __version__
 
+if sys.version_info == (3, 6):
+    INSTALL_REQUIRES = ["dataclasses"]
+else:
+    INSTALL_REQUIRES = []
+
 if __name__ == "__main__":
-    setup(version=__version__)
+    setup(version=__version__, install_requires=INSTALL_REQUIRES)
